6d6d5e7d7cf56d1efbbec7fc8e719a6beeaacace
Nickolas <nulleyson@gmail.com>
2025-05-13 00:51:12 -0300

    Initial commit

---
+++ src/main/java/com/nickolas/api_project_dio/ApiProjectDioApplication.java
@@ -0,0 +1,13 @@
+package com.nickolas.api_project_dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiProjectDioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiProjectDioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nickolas/api_project_dio/ApiProjectDioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nickolas.api_project_dio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiProjectDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
